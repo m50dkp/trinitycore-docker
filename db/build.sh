188c01ebab1ba07b0ab0827cc2802cf8c458194f
Jose Falcon <jose.e.falcon@gmail.com>
2015-01-21 00:13:57 -0500

    Initialize mysql in the dockerfile

--- db/build.sh
+++ db/build.sh
@@ -1,11 +1,23 @@
 export SQL="$TC_DIR/TrinityCore/sql"
+export MYSQL_ROOT_PASSWORD="password"
 
-cat $SQL/create/create_mysql.sql | mysql -hmysql -P3306 -uroot -pGreatBeyond
-cat $SQL/base/auth_database.sql | mysql -hmysql -P3306 -uroot -pGreatBeyond -Dauth
-cat $SQL/base/characters_database.sql | mysql -hmysql -P3306 -uroot -pGreatBeyond -Dcharacters
+# start mysqld in the background
+/docker-entrypoint.sh mysqld_safe &
 
-cat $TC_DIR/sql/TDB_full_*.sql | mysql -hmysql -P3306 -uroot -pGreatBeyond -Dworld
+# we need to wait for that to complete
+mysqladmin --silent --wait=30 ping || exit 1
+
+# now initialize
+cat $SQL/create/create_mysql.sql | mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD"
+
+cat $SQL/base/auth_database.sql | mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" -Dauth
+
+cat $SQL/base/characters_database.sql | mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" -Dcharacters
+
+cat $TC_DIR/sql/TDB_full_*.sql | mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" -Dworld
 
 for file in $SQL/updates/world/*.sql; do
-  cat $file | mysql -hmysql -P3306 -uroot -pGreatBeyond -Dworld
+  cat $file | mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" -Dworld
 done
+
+# TODO: not sure if we need to do the chmods...
