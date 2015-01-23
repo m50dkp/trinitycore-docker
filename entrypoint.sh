6dba82c3c07321a1c89e2ef105d40f9e35977037
Alex Petersen <theoperatore@gmail.com>
2015-01-23 02:09:26 -0500

    added help command and some text

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,18 +21,18 @@ then
   # check to see if the worldserver conf is specified.
   # if not, copy in the default and change the ip address
   # and the data dir for the vmaps and such
-  if [ ! -a '/opt/trinitycore-data/worldserver.conf']; then
+  if [ ! -a '/opt/trinitycore-data/worldserver.conf' ]; then
 
     echo "using default worldserver conf file"
 
     # TODO: give better help
-    if [! -z "$USER_IP_ADDRESS"]; then
+    if [ -z "$USER_IP_ADDRESS" ]; then
       echo >&2 'error: USER_IP_ADDRESS not set'
       echo >&2 '  Specify your ip address'
       exit 1
     fi
 
-    if [! -z "$USER_DATA_DIR"]; then
+    if [ -z "$USER_DATA_DIR" ]; then
       echo >&2 'error: USER_DATA_DIR not set'
       echo >&2 '  Specify the path to maps, mmaps, vmaps, dbc'
       exit 1
@@ -59,11 +59,11 @@ then
   
   # check to see if the authserver conf is specified.
   # if not, copy in the default and change the ip address
-  if [ ! -a '/opt/trinitycore-data/authserver.conf']; then
+  if [ ! -a '/opt/trinitycore-data/authserver.conf' ]; then
     echo "using default auth conf file"
 
     # TODO: give better help
-    if [! -z "$USER_IP_ADDRESS"]; then
+    if [ -z "$USER_IP_ADDRESS" ]; then
       echo >&2 'error: USER_IP_ADDRESS not set'
       echo >&2 '  Specify your ip address'
       exit 1
@@ -79,6 +79,60 @@ then
 
   # RUN. IT.
   /usr/local/bin/authserver -c /opt/trinitycore-data/authserver.conf
+
+elif [ "$CMD" = 'help' ]
+then
+  echo ""
+  echo 'Displaying help for using docker with TrinityCore. I have no clue if'
+  echo 'this is how these commands should be run, but hey. Now we have some'
+  echo 'help text to copy to a real home.'
+  echo ""
+  echo 'SYNOPSIS'
+  echo '  commands [options]'
+  echo ""
+  echo 'COMMANDS'
+  echo '  authserver [--ip-addr <ip-address>]'
+  echo '      Start the authserver. If you do not mount an authserver.conf file,' 
+  echo '      the default will be used with corresponding ip address given by '
+  echo '      --ip-addr flag'
+  echo ""
+  echo '      <ip-address> is the ip address of the computer you are running the warcraft'
+  echo '      server on. Specify a public ip if you want others outside of your local'
+  echo '      network to connect to your server. This must be the same as the'
+  echo '      worldserver ip address.'
+  echo ""
+  echo '  data'
+  echo '      Build container to be used as a data container'
+  echo ""
+  echo '  extract-maps'
+  echo '      Runs a script to extract maps, mmaps, vmaps, and dbc from the'
+  echo '      client. The script assumes that the client is version 3.3.5a'
+  echo '      and that the client is mounted at point /opt/wow-client/'
+  echo ""
+  echo '  help'
+  echo '      Displays this help command. But I guess runs this container'
+  echo '      as well? hmm..something is weird here...maybe this isnt how this is '
+  echo '      supposed to work...'
+  echo ""
+  echo '  update-ip <ip-address>'
+  echo '      Update the ip address of the worldserver and authserver to'
+  echo '      <ip-address>. Specify a public ip address if you want others outside'
+  echo '      of your local network to connect to your server.'
+  echo ""
+  echo '  worldserver [--ip-addr <ip-address> --data-dir <path/to/maps/dir>]'
+  echo '      Start the worldserver. If you do not mount a worldserver.conf file,'
+  echo '      the default will be used. If you plan on using the default configuration'
+  echo '      you must specify both --ip-addr AND --data-dir.'
+  echo ""
+  echo '      <ip-address> is the ip address of the computer you are running the warcraft'
+  echo '      server on. Specify a public ip if you want others outside of your local'
+  echo '      network to connect to your server. This must be the same as the'
+  echo '      authserver ip address.'
+  echo ""
+  echo '      <path/to/maps/dir> is the path to the folder containing the maps, mmaps, vmaps,'
+  echo '      and dbc needed to run the worldserver.'
+  echo ""
+  echo 'END OF HELP good night'
 
 else
 
