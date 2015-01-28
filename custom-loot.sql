dba0f4ca06654cc02f69f872ac6891ee6a453cf5
Jose Falcon <falcon@spotify.com>
2015-01-27 21:50:32 -0500

    wtf

--- custom-loot.sql
+++ custom-loot.sql
@@ -17,3 +17,33 @@ $ mysql -hTCDB -uroot -ppassword
 mysql> use world;
 mysql>    paste your commands here.
 
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS armorDrops;
+CREATE PROCEDURE armorDrops
+(InventoryType tinyint(3),
+ Material tinyint(3))
+BEGIN
+  DROP TEMPORARY TABLE IF EXISTS loot;
+  CREATE TEMPORARY TABLE loot AS
+      SELECT cl.item, cl.Reference FROM creature_template as ct
+      JOIN instance_encounters as ie ON ct.entry = ie.creditEntry
+      JOIN creature_loot_template as cl ON cl.entry = ct.lootid
+      WHERE ie.creditType = 0;
+
+  DROP TEMPORARY TABLE IF EXISTS moreloot;
+  CREATE TEMPORARY TABLE moreloot AS
+    SELECT rl.item FROM reference_loot_template as rl
+    JOIN loot as l ON l.Reference = rl.entry
+    WHERE l.Reference != 0;
+
+  SELECT DISTINCT it.entry, it.name FROM item_template as it
+  WHERE it.class = 4
+  AND it.InventoryType = InventoryType
+  AND it.Material = Material
+  AND (it.entry IN (SELECT item FROM loot) OR it.entry IN (SELECT item FROM moreloot));
+END //
+DELIMITER ;
+
+CALL armorDrops(8, 8);
